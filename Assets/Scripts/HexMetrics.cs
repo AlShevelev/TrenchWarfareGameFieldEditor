065570b82564d47cf333e72693a93930b9395ca8
Alexander Shevelev <al.e.shevelev@gmail.com>
2023-02-13 23:00:36 +0300

    Color blending has been completed

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -37,7 +37,6 @@ public class HexMetrics {
 	}	
 
 	public static Vector3 GetBridge (HexDirection direction) {
-		return (corners[(int)direction] + corners[(int)direction + 1]) *
-			0.5f * blendFactor;
+		return (corners[(int)direction] + corners[(int)direction + 1]) * blendFactor;
 	}	
 }
