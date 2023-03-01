933b5d0ea006b3c11b0d06802542b0ff8927a054
Alexander Shevelev <al.e.shevelev@gmail.com>
2023-03-01 22:12:12 +0300

    Water foam emitaion has been completed

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -38,6 +38,10 @@ public class HexMetrics {
 
 	public const float waterElevationOffset = -0.5f;
 
+	public const float waterFactor = 0.6f;
+
+	public const float waterBlendFactor = 1f - waterFactor;
+
     public static Vector3[] corners = {
 		new Vector3(0f, 0f, outerRadius),
 		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
@@ -115,5 +119,17 @@ public class HexMetrics {
 		position.z += (sample.z * 2f - 1f) * HexMetrics.cellPerturbStrength;
 		
 		return position;
+	}
+
+	public static Vector3 GetFirstWaterCorner (HexDirection direction) {
+		return corners[(int)direction] * waterFactor;
+	}
+
+	public static Vector3 GetSecondWaterCorner (HexDirection direction) {
+		return corners[(int)direction + 1] * waterFactor;
+	}
+
+	public static Vector3 GetWaterBridge (HexDirection direction) {
+		return (corners[(int)direction] + corners[(int)direction + 1]) * waterBlendFactor;
 	}
 }
--- Assets/Shaders/Water Shore.shader
+++ Assets/Shaders/Water Shore.shader
@@ -43,6 +43,8 @@ Shader "Custom/Water Shore"
         void surf (Input IN, inout SurfaceOutputStandard o)
         {
             float shore = IN.uv_MainTex.y;
+            shore = sqrt(shore) * 0.9;
+
 			float foam = Foam(shore, IN.worldPos.xz, _MainTex);
 			float waves = Waves(IN.worldPos.xz, _MainTex);
 			waves *= 1 - shore;
