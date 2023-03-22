4792aa85becd8f9bd978538154115c47195e7250
Alexander Shevelev <al.e.shevelev@gmail.com>
2023-03-22 22:28:42 +0300

    Cell textures suppors has been completed

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -6,13 +6,15 @@ using System.Collections.Generic;
 public class HexMesh : MonoBehaviour {
 	Mesh hexMesh;
 
-	[NonSerialized] List<Vector3> vertices;
+	[NonSerialized] List<Vector3> vertices, terrainTypes;
 	[NonSerialized] List<Color> colors;
 	[NonSerialized] List<int> triangles;
 	
 	MeshCollider meshCollider;
 
 	public bool useCollider, useColors, useUVCoordinates, useUV2Coordinates;
+
+	public bool useTerrainTypes;
 
 	[NonSerialized] List<Vector2> uvs, uv2s;
 
@@ -149,6 +151,19 @@ public class HexMesh : MonoBehaviour {
 		uv2s.Add(new Vector2(uMax, vMax));	
 	}
 
+	public void AddTriangleTerrainTypes (Vector3 types) {
+		terrainTypes.Add(types);
+		terrainTypes.Add(types);
+		terrainTypes.Add(types);
+	}
+
+	public void AddQuadTerrainTypes (Vector3 types) {
+		terrainTypes.Add(types);
+		terrainTypes.Add(types);
+		terrainTypes.Add(types);
+		terrainTypes.Add(types);
+	}
+
 	public void Clear () {
 		hexMesh.Clear();
 
@@ -163,6 +178,10 @@ public class HexMesh : MonoBehaviour {
 
 		if (useUV2Coordinates) {
 			uv2s = ListPool<Vector2>.Get();
+		}
+
+		if (useTerrainTypes) {
+			terrainTypes = ListPool<Vector3>.Get();
 		}
 
 		triangles = ListPool<int>.Get();
@@ -187,6 +206,11 @@ public class HexMesh : MonoBehaviour {
 			ListPool<Vector2>.Add(uv2s);
 		}
 		
+		if (useTerrainTypes) {
+			hexMesh.SetUVs(2, terrainTypes);
+			ListPool<Vector3>.Add(terrainTypes);
+		}
+
 		hexMesh.SetTriangles(triangles, 0);
 		ListPool<int>.Add(triangles);
 
